0753cde365e058d962028549deda45f07a2e1aeb
SpaceX120892 <info.laptrinh2s@gmal.com>
2016-07-08 13:57:54 +0700

    căn giũa thông tin liên hê

--- Dong/view/header.php
+++ Dong/view/header.php
@@ -1,8 +1,8 @@
 <div class="header_thong_tin_lien_he">
 	<table>
 		<tr>
-			<td width="150px">08.06680.9686   |</td>
-			<td width="150px">0934.515.339   |</td>
+			<td width="150px">08.06680.9686</td>
+			<td width="150px">0934.515.339</td>
 			<td width="550px">Lầu 3 - Tòa nhà Lữ Gia - Số 70 Lữ Gia - P.15 - Q.11 - TP. HCM</td>
 		</tr>
 	</table>
